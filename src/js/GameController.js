dd54f0a7e51f181ab68f9adab9d7dbb9673cf97c
Ksenia Timokhina <kseenina@gmail.com>
2026-09-19 21:19:26 +0300

    Step 4: “Character Selection” and the tests for it have been completed

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -7,12 +7,15 @@ import Daemon from "./characters/Daemon.js";
 import { generateTeam } from "./generators.js";
 import PositionedCharacter from "./PositionedCharacter.js";
 import themes from "./themes.js";
+import GameState from "./GameState.js";
+import GamePlay from "./GamePlay.js";
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.level = 1;
+    this.gameState = new GameState();
   }
 
   init() {
@@ -62,14 +65,32 @@ export default class GameController {
   subscribeToEvents() {
     this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
     this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
   }
 
   formatCharacterInfo(character) {
     return `🎖${character.level} ⚔${character.attack} 🛡${character.defence} ❤${character.health}`;
   }
 
-  onCellClick(index) {
-    // TODO: react to click
+  onCellClick(index) {    
+    const position = this.positions.find(item => item.position === index);
+    if (!position) {
+      GamePlay.showError('Тут никого нет! :(');
+      return;
+    }
+    const character = position.character;
+    const playerTypes = ['bowman', 'swordsman', 'magician'];
+
+    if(!playerTypes.includes(character.type)) {
+      GamePlay.showError('Это не твой боец, не трожь!');
+      return;
+    }
+    if (this.gameState.selectedCell !== null) {
+      const prevIndex = this.gameState.selectedCell;
+      this.gamePlay.deselectCell(prevIndex);
+    }
+    this.gamePlay.selectCell(index);
+    this.gameState.selectedCell = index;
   }
 
   onCellEnter(index) {
--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,4 +1,13 @@
 export default class GameState {
+  constructor() {
+    this.playerTurn = true;
+    this.selectedCell = null;
+  }
+
+  toggleTurn() {
+    this.playerTurn = !this.playerTurn;
+  }
+  
   static from(object) {
     // TODO: create object
     return null;
